a3d17b855c78c720437a3980bc3735bd3fcb7af4
chrislucas <christoffer.luccas@gmail.com>
2018-03-22 18:26:40 -0300

    comecando os estudos pelo padrao factory para oconstrucao de objetos, seguindo os exemplos do livro

---
+++ DesignPatternOO/app/src/main/java/br/com/xplorer/designpatternoo/entities/Baguette.java
@@ -0,0 +1,10 @@
+package br.com.xplorer.designpatternoo.entities;
+
+import br.com.xplorer.designpatternoo.factory.Bread;
+
+/**
+ * Created by r028367 on 22/03/2018.
+ */
+
+public class Baguette implements Bread {
+}
---
+++ DesignPatternOO/app/src/main/java/br/com/xplorer/designpatternoo/factory/Bread.java
@@ -0,0 +1,8 @@
+package br.com.xplorer.designpatternoo.factory;
+
+/**
+ * Created by r028367 on 22/03/2018.
+ */
+
+public interface Bread {
+}
